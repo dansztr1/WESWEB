4e96d9f78ec55eec3ebc69cd5569340421035c5d
Daniel <southcoastrailrbx@gmail.com>
2025-09-30 09:14:37 +0200

    Node

---
+++ Node/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+
+app.get("/", (request, response) => {
+    ejs.readFile('./home.html', "utf8", (err, html)  => {
+        if (err) {
+            response.status(500).send("Internal server error")
+        }
+        response.send(html);
+    })
+});
+
+app.listen(3000, () => console.log('App Running on http://localhost:3000'))
